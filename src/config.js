0711452dbaf07768024861585bc92f09d653867d
tboy4all <taiyesuleiman09@gmail.com>
2025-07-08 23:06:57 +0100

    added env file to vercel

--- src/config.js
+++
@@ -1,1 +0,0 @@
-export const API_BASE_URL = 'https://igaming-backend-0y7x.onrender.com'
--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,9 @@
 import axios from 'axios'
 
+const baseURL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api'
+
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api', // Adjust if hosted elsewhere
+  baseURL,
 })
 
 export default API
